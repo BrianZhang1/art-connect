cfb4a5837e3ccf6b4f69688c6d30beec17a21f82
Siddh Patel <47033295+siddhp1@users.noreply.github.com>
2024-05-04 16:33:27 -0400

    added more events

--- app/data.js
+++ app/data.js
@@ -1,7 +1,7 @@
 export const events = [
   {
     id: 1,
-    name: "Brampton Music Festival",
+    name: "Street Drum Performance",
     type: "Music",
     coords: { lat: 43.68337579543226, lng: -79.75726497597383 },
   },
@@ -16,5 +16,23 @@ export const events = [
     name: "Brampton Comedy Night",
     type: "Stand-up Comedy",
     coords: { lat: 43.6873273107782, lng: -79.76005453006361 },
+  },
+  {
+    id: 4,
+    name: "Brampton Music Festival",
+    type: "Music",
+    coords: { lat: 43.68192535105397, lng: -79.81572125850563 },
+  },
+  {
+    id: 5,
+    name: "Brampton Garba",
+    type: "Dance",
+    coords: { lat: 43.743786619646656, lng: -79.76944726488436 },
+  },
+  {
+    id: 6,
+    name: "Brampton Comedy Night",
+    type: "Stand-up comedy",
+    coords: { lat: 43.78472713892429, lng: -79.67745797893511 },
   },
 ];
